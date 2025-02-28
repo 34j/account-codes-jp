716c60f66f221d8b3237ae953c3a8cdb5e5c2831
34j <34j.github@proton.me>
2025-02-28 17:14:05 +0900

    chore: wip

--- src/account_codes_jp/_blue_return.py
+++ src/account_codes_jp/_blue_return.py
@@ -1,7 +1,43 @@
+from collections.abc import Mapping, Sequence
 from pathlib import Path
+from typing import Any
 
 import networkx as nx
 from yaml import safe_load
+
+
+def from_nested_dict(
+    data: Mapping[Any, Mapping[Any, Any] | Sequence[Any]],
+) -> nx.DiGraph:
+    """
+    Convert a nested mapping of mappings and sequences to a directed graph.
+
+    Parameters
+    ----------
+    data : Mapping[Any, Mapping[Any, Any] | Sequence[Any]]
+        Nested dictionary
+
+    Returns
+    -------
+    nx.DiGraph
+        Directed graph
+
+    """
+    G = nx.DiGraph()
+    q: list[tuple[Any, Any]] = [(None, data)]
+    id_child = 0
+    while q:
+        id_parent, d = q.pop()
+        for k_child, grandchildren in d.items():
+            id_child += 1
+            G.add_node(id_child, label=k_child)
+            if id_parent is not None:
+                G.add_edge(id_parent, id_child)
+            if isinstance(grandchildren, Mapping):
+                q.append((id_child, grandchildren))
+            elif isinstance(grandchildren, list):
+                q.append((id_child, {k: None for k in grandchildren}))
+    return G
 
 
 def get_blue_return_accounts_as_graph() -> nx.DiGraph:
@@ -16,7 +52,7 @@ def get_blue_return_accounts_as_graph() -> nx.DiGraph:
     """
     with (Path(__file__).parent / "_blue_return.yaml").open(encoding="utf-8") as f:
         d = safe_load(f)
-    G = nx.DiGraph(d)
+    G = from_nested_dict(d)
     for n in G.nodes:
         G.nodes[n]["abstract"] = False
         G.nodes[n]["title"] = False
