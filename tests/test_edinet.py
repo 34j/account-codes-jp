42fe41bfc323d0e89543be8f2c425331e02ddf8b
34j <34j.github@proton.me>
2025-03-02 15:18:03 +0900

    test: reduce requests

--- tests/test_edinet.py
+++ tests/test_edinet.py
@@ -1,7 +1,17 @@
+from os import environ
+from sys import platform, version_info
+
+import pytest
+
 from account_codes_jp import AccountType, get_account_type_factory, get_edinet_accounts
 
 
 def test_get():
+    if environ.get("CI") == "true":
+        if version_info.minor == 12 and platform == "linux":
+            pass
+        else:
+            pytest.skip("Skip the test that requires the Internet.")
     G = get_edinet_accounts()
     t = get_account_type_factory(G)
     assert t("現金") == AccountType.Asset
