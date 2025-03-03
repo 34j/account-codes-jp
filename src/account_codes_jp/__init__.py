e20b63da56a55fcc5e5593f10902372f5461dc8d
34j <34j.github@proton.me>
2025-03-03 16:17:25 +0900

    feat: add ambiguous factory

--- src/account_codes_jp/__init__.py
+++ src/account_codes_jp/__init__.py
@@ -6,6 +6,7 @@ from ._common import (
     Account,
     AccountSundry,
     AccountType,
+    get_account_ambiguous_factory,
     get_account_type_factory,
 )
 from ._edinet import ETaxAccountProtocol, Industry, get_edinet_accounts
@@ -17,6 +18,7 @@ __all__ = [
     "AccountType",
     "ETaxAccountProtocol",
     "Industry",
+    "get_account_ambiguous_factory",
     "get_account_type_factory",
     "get_blue_return_accounts",
     "get_edinet_accounts",
--- tests/test_aoiro.py
+++ tests/test_aoiro.py
@@ -1,11 +1,18 @@
+import pytest
+
 from account_codes_jp import (
     AccountType,
+    get_account_ambiguous_factory,
     get_account_type_factory,
     get_blue_return_accounts,
 )
 
 
-def test_get():
+@pytest.mark.parametrize(
+    "ambiguous",
+    [False, True],
+)
+def test_get(ambiguous: bool) -> None:
     G = get_blue_return_accounts()
     t = get_account_type_factory(G)
     assert t("現金") == AccountType.Asset
@@ -13,3 +20,10 @@ def test_get():
     assert t("元入金") == AccountType.Equity
     assert t("消耗品費") == AccountType.Expense
     assert t("売上") == AccountType.Revenue
+
+
+def test_amb() -> None:
+    G = get_blue_return_accounts()
+    a = get_account_ambiguous_factory(G)
+    assert a("定!!!預金") == "定期預金"
+    assert a("しんぶんひ") == "新聞図書費"
