0a0821f8edbad525db03d7eb6d01cf7f541a53ef
34j <34j.github@proton.me>
2025-03-03 19:01:26 +0900

    fix(cli): remove unnecessary keywords

--- src/account_codes_jp/cli.py
+++ src/account_codes_jp/cli.py
@@ -47,7 +47,7 @@ def list(
                 f"{d['label']}/[green]{d['label_etax']}[/green]/"
                 f"[sky_blue3]{d['prefix']}:{d['element']}[/sky_blue3]"
             )
-    for i, line in enumerate(generate_network_text(G, with_labels=True, max_depth=20)):
+    for i, line in enumerate(generate_network_text(G, with_labels=True)):
         if i <= 0:
             continue
         print(line)
